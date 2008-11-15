0a49cb87e1a44373fb8035855b74219fbb471c94
virtualmachine <virtualmachine@virtualmachine-laptop.(none)>
2008-11-15 00:33:22 -0500

    added simulate.c from project 1.

---
+++ simulator/simulate.c
@@ -0,0 +1,196 @@
+/* EECS 370 LC-2K8 Instruction-level simulator */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUMMEMORY 65536 /* maximum number of words in memory */
+#define NUMREGS 8 /* number of machine registers */
+#define MAXLINELENGTH 1000	
+
+#define M_OPCODE 29360128
+#define M_REGA 3670016
+#define M_REGB 458752
+#define M_DESTREG 7
+#define M_OFFSETFIELD 65535
+
+#define B_OPCODE 22
+#define B_REGA 19
+#define B_REGB 16
+#define B_DESTREG 0
+#define B_OFFSETFIELD 0
+
+#define I_ADD	0
+#define	I_NAND	1
+#define I_LW	2
+#define I_SW	3
+#define	I_BEQ	4
+#define	I_JALR	5
+#define	I_HALT	6
+#define	I_NOOP	7
+
+typedef struct stateStruct {
+    int pc;
+    int mem[NUMMEMORY];
+    int reg[NUMREGS];
+    int numMemory;
+} stateType;
+
+void printState(stateType *);
+int convertNum(int num);
+
+void initialize(stateType *);
+void simulate(stateType *);
+
+int getOpcode(int num);
+int getRegA(int num);
+int getRegB(int num);
+int getOffsetField(int num);
+
+
+int
+main(int argc, char *argv[])
+{
+    char line[MAXLINELENGTH];
+    stateType state;
+    FILE *filePtr;
+
+    if (argc != 2) {
+	printf("error: usage: %s <machine-code file>\n", argv[0]);
+	exit(1);
+    }
+
+    filePtr = fopen(argv[1], "r");
+    if (filePtr == NULL) {
+	printf("error: can't open file %s", argv[1]);
+	perror("fopen");
+	exit(1);
+    }
+
+    /* read in the entire machine-code file into memory */
+    for (state.numMemory = 0; fgets(line, MAXLINELENGTH, filePtr) != NULL;
+	state.numMemory++) {
+	
+		if (sscanf(line, "%d", state.mem+state.numMemory) != 1) {
+		printf("error in reading address %d\n", state.numMemory);
+		exit(1);
+		}
+	
+	printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
+    }
+	initialize(&state);
+	simulate(&state);
+    return(0);
+}
+
+void
+printState(stateType *statePtr)
+{
+    int i;
+    printf("\n@@@\nstate:\n");
+    printf("\tpc %d\n", statePtr->pc);
+    printf("\tmemory:\n");
+	for (i=0; i<statePtr->numMemory; i++) {
+	    printf("\t\tmem[ %d ] %d\n", i, statePtr->mem[i]);
+	}
+    printf("\tregisters:\n");
+	for (i=0; i<NUMREGS; i++) {
+	    printf("\t\treg[ %d ] %d\n", i, statePtr->reg[i]);
+	}
+    printf("end state\n");
+}
+
+void 
+simulate(stateType * state)
+{
+	int abort = 0;
+	int inst, opcode, i_regA, i_regB, i_destReg, offsetField, v_regA, v_regB;
+	int num_insts = 0;
+	while(!abort)
+	{
+		printState(state);
+		inst = state->mem[state->pc];
+		opcode = (inst & M_OPCODE) >> B_OPCODE;
+		i_regA = (inst & M_REGA) >> B_REGA;
+		i_regB = (inst & M_REGB) >> B_REGB;
+		i_destReg = (inst & M_DESTREG) >> B_DESTREG;
+		offsetField = convertNum(inst & M_OFFSETFIELD) >> B_OFFSETFIELD;
+		
+		v_regA = state->reg[i_regA];
+		v_regB = state->reg[i_regB];
+		
+		if (opcode == I_ADD)
+		{
+			state->reg[i_destReg] = v_regA + v_regB;
+		}
+		
+		else if (opcode == I_NAND)
+		{
+			state->reg[i_destReg] = ~(v_regA & v_regB);
+		}
+
+		else if (opcode == I_LW)
+		{
+			state->reg[i_regB] = state->mem[v_regA + offsetField];
+		}
+		
+		else if (opcode == I_SW)
+		{
+			state->mem[v_regA + offsetField] = v_regB;
+		}
+		
+		else if (opcode == I_BEQ)
+		{
+			if (v_regA == v_regB)
+				state->pc += offsetField;
+		}
+		
+		else if (opcode == I_JALR)
+		{
+			state->reg[i_regB] = state->pc + 1;
+			state->pc  = v_regA - 1;
+		}
+		
+		else if (opcode == I_HALT)
+		{
+			abort = 1;
+		}
+		
+		else if (opcode == I_NOOP)
+		{
+			//Doing nothing. Whee.
+		}
+		
+		num_insts++;
+		state->pc++;
+	}
+	
+	printf("machine halted\n");
+	printf("total of %d instructions executed\n", num_insts);
+	printf("final state of machine:\n");
+	
+	printState(state);
+}
+
+void
+initialize(stateType * state)
+{
+	int i = 0;
+	for (i=0; i<NUMREGS; i++) 
+	{
+	    state->reg[i] = 0;
+	}
+
+	state->pc = 0;
+}
+
+
+int
+convertNum(int num)
+{
+	/* convert a 16-bit number into a 32-bit Sun integer */
+	if (num & (1<<15) ) {
+		num -= (1<<16);
+	}
+	return(num);
+}
