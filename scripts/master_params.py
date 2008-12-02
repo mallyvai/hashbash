5de2d9ee062f884d25c463e37082df31e394a76d
virtualmachine <virtualmachine@virtualmachine-laptop.(none)>
2008-12-01 23:14:25 -0500

    modified params for actual operation.

--- scripts/master_params.py
+++ scripts/master_params.py
@@ -1,7 +1,7 @@
 computation_directory = "/tmp/testgen"
 
 #The maximum number of programs that can be in a given generation
-max_num_functions = lambda : 10
+max_num_functions = lambda : 20
 #The minimum number of functions that have to be new
 min_new_functions = lambda : int(.1 * max_num_functions())
 max_new_functions = lambda : int(0.5 * max_num_functions())
@@ -10,7 +10,7 @@ max_bred_functions = lambda : max_num_functions() - min_new_functions()
 min_bred_functions = lambda : max_num_functions() - max_new_functions()
 
 #Maximum number of allowed workers
-max_num_workers = 20
+max_num_workers = 10
 
 mcode_suffix = ".mc"
 fit_suffix = ".fit"
