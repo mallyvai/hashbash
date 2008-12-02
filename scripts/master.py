bda6404d70b80fd48035a494cfed2f63b1af1e10
virtualmachine <virtualmachine@virtualmachine-laptop.(none)>
2008-12-01 21:42:20 -0500

    REALLY REALLY CLOSE......

--- scripts/master.py
+++ scripts/master.py
@@ -1,19 +1,29 @@
-import processing as proc
-import master_params as mp
 import os
-import generate
-import simulate
 import random
 import tempfile
 import shutil
+import glob
+import sys
+
+import generate
+import simulate
 import fitness
 import breed
-import glob
 
-path_dir = lambda generation: os.path.abspath(os.path.join(mp.computation_directory, str(generation)+mp.generation_suffix))
+import processing as proc
+import master_params as mp
+import gen_params as gp
+
+def gen_dir(gen_num):
+	base = str(gen_num) + mp.generation_suffix
+	if gen_num is None or len(str(gen_num)) == 0:
+		base = ""
+	return os.path.abspath(os.path.join(mp.computation_directory, base))
+	
+gen_member = lambda generation, num: os.path.join(gen_dir(generation), str(num) + mp.mcode_suffix)
 
 def make_next_dir(gen_num):
-	dirname = path_dir(str(gen_num))
+	dirname = gen_dir(str(gen_num))
 	os.mkdir(dirname)
 	return dirname
 
@@ -30,7 +40,8 @@ def find_unmeasured(path):
 	
 	return unmeasured
 
-def filtered_filenames(path):
+def filtered_filenames(num):
+	path = gen_dir(num)
 	filenames = os.listdir(path)
 	filenames = [os.path.join(path, i) for i in filenames]
 	candidates = {}
@@ -49,13 +60,13 @@ def filtered_filenames(path):
 	
 	#candidates that have information about the (very expensive) randomness test
 	advanced_candidates = []
-	for candidate, fitness in candidates.iteritems():
-		if len(fitness) > 2: #hacky hacky\
-			advanced_candidates.append((candidate, fitness))
+	for candidate, fit in candidates.iteritems():
+		if len(fit) > 2: #hacky hacky\
+			advanced_candidates.append((candidate, fit))
 			del candidates[candidate]
 	
 	
-	basic_candidates = [(candidate, fitness) for candidate, fitness in candidates.iteritems()]
+	basic_candidates = [(candidate, fit) for candidate, fit in candidates.iteritems()]
 	
 	basic_candidates.sort(lambda x, y: cmp(x[1][mp.li_trivial], y[1][mp.li_trivial]))
 	
@@ -63,7 +74,7 @@ def filtered_filenames(path):
 	filtered_candidates = advanced_candidates + basic_candidates
 	
 	print filtered_candidates
-	return [i[0] for i in filtered_candidates]
+	return [i[0]+mp.mcode_suffix for i in filtered_candidates]
 
 def choose_partners(ordered_candidates):
 	"""
@@ -78,18 +89,30 @@ def choose_partners(ordered_candidates):
 	return final_candidates
 
 def breed_fittest(partner_filenames):
-	fh = open(partner_filenames[0], 'r')
+	print "MADE IT THIS FAR"
+	print partner_filenames
+	
+	one_name = partner_filenames[0]
+	two_name = partner_filenames[1]
+	
+	fh = open(one_name, 'r')
 	one = fh.readlines()	
 	fh.close()
 	
-	fh = open(partner_filenames[1], 'r')
+	fh = open(two_name, 'r')
 	two = fh.readlines()
 	fh.close()
 	
-	child = double_concatenation(one, two)
+	print "pre-breeding"
 	
-	#append the child's parents as a comment.
-	child.append(''.join(["\n#",one, ";", two]))
+	child = breed.double_concatenation(one, two)
+	
+	print "post-breeding"
+	
+	#Append the child's parents as a comment.
+	
+	child.append(''.join(["\n#",one_name, ";", two_name]))
+	print "done"
 	return child
 
 def measure_program(filename):
@@ -117,7 +140,7 @@ def measure_program(filename):
 
 def initialize():
 	make_next_dir(0)
-	path = path_dir(0)
+	path = gen_dir(0)
 	for i in xrange(mp.max_num_functions()):
 		filename = os.path.join(path, str(i)+mp.mcode_suffix)
 		generate.main(filename)
@@ -127,9 +150,10 @@ def initialize():
 """
 
 def measure_generation(num):
-	path = path_dir(num)
 	pool = proc.Pool(mp.max_num_workers)
-	
+
+	path = gen_dir(num)
+	print "measuring path"
 	unmeasured = find_unmeasured(path)
 	pool.map(measure_program, unmeasured)
 	
@@ -138,40 +162,76 @@ def measure_generation(num):
 has been fully handled, and creating new chromosomes to fill in the gaps.
 """
 
+"""
+Creates a generation directory with num
+"""
 def create_generation(num):
-	if num < 0: raise IndexError(str(num) + " is too small. Must be >= 0.")
+	if num <= 0: raise IndexError(str(num) + " is too small. Must be > 0.")
+	pool = proc.Pool(mp.max_num_workers)
 	
 	#We now have fitness function output for every program in this directory.
 	#Lets arrange them by the fittest discarding the stupid ones.
-	ordered_candidates = filtered_filenames(prev)
+	ordered_candidates = filtered_filenames(str(num-1))
 	partners = choose_partners(ordered_candidates)
-
+	print "PARTNERS------------", partners
 	#Breeding time.
+	
 	result = pool.map_async(breed_fittest, partners)
 	result.wait()
 	children = result.get()
 	
-	final = []
-	num_randgen = random.randint(mp.min_new_functions, mp.max_new_functions)
+	num_randgen = random.randint(mp.min_new_functions(), mp.max_new_functions())
 	for i in xrange(num_randgen):
-		final.append(generate(gp.num_rnd_instrs))
+		children.append(generate.generate(gp.num_rnd_instrs))
+	
+	os.mkdir(gen_dir(num))
+	
+	for child, i in zip(children, xrange(len(children))):
+		fh = open(gen_member(num, i), 'w')
+		child = [str(word) for word in child]
+		child.append("\n")
+		fh.write(''.join(child))
+		fh.close()
+	
+class NoGenerationsExist(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
 
 #Returns None, None if no generations exist
 #Otherwise return biggest_path, biggest_num
 def find_biggest_generation():
-	path = path_dir("")
-	contents = glob.glob(path_dir("*"+mp.generation_suffix))
+	path = gen_dir("")
+	contents = glob.glob(gen_dir("*"))
 	contents.sort(reverse=True)
-	contents = [os.path.join(path, i) for i in contents]
-	if len(contents) == 0: return None, None
-	return contents[0], os.path.basename(contents[0])[:-len(mp.generation_suffix)]
+	if len(contents) == 0: raise NoGenerationsExist(path)
+	return contents[0], int(os.path.basename(contents[0])[:-len(mp.generation_suffix)])
 
 def main(additional_generations_to_create):
-	if num < 0: raise IndexError(str(num) + " is too small. Must be >= 0.")
-	biggest_path, biggest_num = find_biggest_generation()
-	for i in xrange(biggest_num+1, biggest_num + additional_generations_to_create):
-		handle_generation(i)
+	if additional_generations_to_create < 0: raise IndexError(str(additional_generations_to_create) + " is too small. Must be >= 0.")
+	
+	biggest_path, biggest_num = None, None
+	
+	try:
+		biggest_path, biggest_num = find_biggest_generation()
+	except NoGenerationsExist:
+		initialize()
+		biggest_path, biggest_num = find_biggest_generation()
+
+	print biggest_path, biggest_num
+	for i in xrange(biggest_num, biggest_num + additional_generations_to_create):
+		measure_generation(i)
 		create_generation(i+1)
 		
 if __name__ == "__main__":
-	main(1)
+	
+	try:
+		print sys.argv[1]
+		if sys.argv[1] == "-f":
+			shutil.rmtree(mp.computation_directory)
+			os.mkdir(mp.computation_directory)
+	except IndexError: None
+	
+	main(2)
